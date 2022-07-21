b7dddc461d3dbe2ccce0ab38d7fde49d90b1e23c
topeadeolatunji <104689265+topeadeolatunji@users.noreply.github.com>
2022-07-21 19:48:41 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+let square = (num) = num**2;
+console.log(square(19))
+
+
