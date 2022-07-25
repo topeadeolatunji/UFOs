c90f66b4fe85a2f766f4c238306a809211e4fb68
topeadeolatunji <104689265+topeadeolatunji@users.noreply.github.com>
2022-07-24 20:39:03 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
 // import the data from data.js
-const tableData = data;
+//const tableData = data;
 
 // Reference the HTML table using d3
-var tbody = d3.select("tbody");
+//var tbody = d3.select("tbody");
 
-let square = (num) = num**2;
-console.log(square(19))
+//let square = (num) = num**2;
+//console.log(square(19))
 
 
+// Converted to an arrow function
+//addition = (a, b) => a + b;
+
+let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+ }
+ listLoop(friends)
+
+ var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+}
+
+for (var i = 0; i < 5; i++) {
+    console.log("I am " + i);
+ }
+
+ function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
